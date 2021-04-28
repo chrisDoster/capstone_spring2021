cc832eee11f30701055d357cd4564cde21a8a445
Tanner Coleman <tannerman37@gmail.com>
2021-04-27 20:11:35 -0500

    Major problems with tokens

--- main.py
+++ main.py
@@ -12,6 +12,28 @@ import os
 from flask import Flask, session, request, redirect
 from flask_session import Session
 import uuid
+import webbrowser
+import sys
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+import logging
+
+# TWITTER
+# for cnd243@twitter.com (chris) ; NOTE: I only follow 1 person atm, an author named Ursula K Le Guin
+twitter_api_key = "eu0hXaGudKMlb93aAlIbJBYF1"
+twitter_api_key_secret = "tywK4Vzses8Pt0GprxWUuDQJ0G3aRKARmBN1cXdO2Kp3r9syTn"
+twitter_access_token = "1364394392577056773-HD04xN1RPm8g9uE5jRU5RTZS2kA3w7"
+twitter_access_token_secret = "VLbcymTCK8RkvpCdsCwZCyADthXtbbhSClZw6zmnGPVtZ"
+
+# authenticate to twitter
+auth = tweepy.OAuthHandler(twitter_api_key, twitter_api_key_secret)
+auth.set_access_token(twitter_access_token, twitter_access_token_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+tl = api.home_timeline()
+for tweet in tl:
+    print(f"{tweet.user.name} said: {tweet.text}")
 
 # Token System
 os.environ['SPOTIPY_CLIENT_ID'] = '7955beeac6734d048ebd62498bb8cd05'
@@ -28,6 +50,17 @@ Session(app)
 caches_folder = './.spotify_caches/'
 if not os.path.exists(caches_folder):
     os.makedirs(caches_folder)
+
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["200 per day", "50 per hour"]
+)
+
+@app.route("/exempt")
+@limiter.exempt
+def exempt():
+    return "No limits!"
 
 def session_cache_path():
     return caches_folder + session.get('uuid')
@@ -52,7 +85,7 @@ def index():
     return f'<h2>Hi {spotify.me()["display_name"]}, ' \
            f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
            f'<a href="/playlists">my playlists</a> | ' \
-           f'<a href="/create_playlists">create new playlists</a> | ' \
+           f'<a href="/create_playlist">create new playlists</a> | ' \
             f'<a href="/current_user">me</a>' \
 
 @app.route('/sign_out')
@@ -63,6 +96,7 @@ def sign_out():
     except OSError as e:
         print("Error: %s - %s." % (e.filename, e.strerror))
     return redirect('/')
+    return 'OK'
 
 @app.route('/playlists')
 def playlists():
@@ -72,46 +106,20 @@ def playlists():
         return redirect('/')
 
     spotify = spotipy.Spotify(auth_manager=auth_manager)
-    return spotify.current_user_playlists()
-
-
-# TWITTER
-# for cnd243@twitter.com (chris) ; NOTE: I only follow 1 person atm, an author named Ursula K Le Guin
-twitter_api_key = "eu0hXaGudKMlb93aAlIbJBYF1"
-twitter_api_key_secret = "tywK4Vzses8Pt0GprxWUuDQJ0G3aRKARmBN1cXdO2Kp3r9syTn"
-twitter_access_token = "1364394392577056773-HD04xN1RPm8g9uE5jRU5RTZS2kA3w7"
-twitter_access_token_secret = "VLbcymTCK8RkvpCdsCwZCyADthXtbbhSClZw6zmnGPVtZ"
-
-# authenticate to twitter
-auth = tweepy.OAuthHandler(twitter_api_key, twitter_api_key_secret)
-auth.set_access_token(twitter_access_token, twitter_access_token_secret)
-
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
-tl = api.home_timeline()
-for tweet in tl:
-    print(f"{tweet.user.name} said: {tweet.text}")
-
-
-# SPOTIFY
-# for BerndBeats on Spotify
-# spotify_client_id = '7595beeac6734d048ebd62498bb8cd05'
-# spotify_client_secret = '96a99012fd0843d39434188e6356fbc6'
-# spotify_client_redirect_uri = 'http://localhost:8080'
-# user_id = "1263578962"
-# scope = 'playlist-modify-private'
-# token = util.prompt_for_user_token(user_id, scope, spotify_client_id, spotify_client_secret, spotify_client_redirect_uri)
-# spo = spotipy.Spotify(auth = token)
-# spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=spotify_client_id, client_secret=spotify_client_secret))
+    webbrowser.open(f"https://open.spotify.com/")
+    return 'OK'
 
 # SPOTIFY
 # Creating recommended playlist
+@app.route('/create_playlist')
 def create_playlist():
     cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
-    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='playlist-modify-private', cache_handler=cache_handler, show_dialog=True)
     if not auth_manager.validate_token(cache_handler.get_cached_token()):
         return redirect('/')
+
     spotify = spotipy.Spotify(auth_manager=auth_manager)
+    token = 'BQCQl_VEaakS__RRtt_ChBkbVlOod_zNZkWScJAkwpsR39nwZievRmWupMq9VIvVFX0BICyHeU8-gKABk6ed5S1z-LWIOfnMVag5k3uBqWOHg_Aa6WKjFoV9xQEat98aMjX13Q6yBIpfbNTcOyR4X1JHbMxGRqHFKBKCddYpgvyp8as'
 
     endpoint_url = "https://api.spotify.com/v1/recommendations?"
     limit = 10
@@ -120,21 +128,16 @@ def create_playlist():
     myPlaylist = []
 
     query = f'{endpoint_url}limit={limit}&market={market}&seed_genres={seed_genres}'
-    response = requests.get(query, headers = {"Content-Type":"application/json", "Authorization":f"Bearer {spotify.get_access_token()}"})
+    response = requests.get(query, headers = {"Content-Type":"application/json", "Authorization":f"Bearer {token}"})
 
     json_response = response.json()
-    for i in json_response['tracks']:
-        myPlaylist.append(i)
-        return(f'<h2>{i.name}</h2>')
+    tracks = json_response.get('items', [])
 
-        return f'<h2>Hi {spotify.me()["display_name"]}, ' \
-           f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
-           f'<a href="/playlists">my playlists</a> | ' \
-           f'<a href="/create_playlists">create new playlists</a> | ' \
-            f'<a href="/current_user">me</a>' \
-        #print(f"\"{i['name']}\" by {i['artists'][0]['name']}")
+    for i, j in enumerate(json_response['tracks']):
+        myPlaylist.append(j['uri'])
+        print(myPlaylist[i])
 
-    # Creating playlist of recommended songs on spotify account
+    # Creating playlist for the recommended songs
     endpoint_url = f"https://api.spotify.com/v1/users/{spotify.current_user()}/playlists"
     request_body = json.dumps({
         "name": "My private playlist",
@@ -142,34 +145,55 @@ def create_playlist():
         "public": False
     })
     response = requests.post(url = endpoint_url, data = request_body, 
-                            headers = {"Content-Type":"application/json", "Authorization":f"Bearer {spotify.get_access_token()}"})
+                            headers = {"Content-Type":"application/json", "Authorization":f"Bearer {token}"})
 
-    url = response.json()['external_urls']['spotify']
     print(response.status_code)
-
-    # Filling new playlist with recommended songs
-    playlist_id = response.json()['id']
+    playlist_id = json_response.get('id', [])
+    print(str(playlist_id))
     endpoint_url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
     request_body = json.dumps({
-        "myPlaylist" : myPlaylist
+    "myPlaylist" : myPlaylist
     })
+    # url = resp.get('external_urls','spotify')
+    # print(response.status_code)
+    #playlist_name = "Random playlist"
+    #playlist_id = ''
+    #playlist = spotify.user_playlist_create(spotify.current_user(), name=playlist_name, public=False)
+    # for playlist in playlists['items']:
+    #     if playlist['name'] == playlist_name:
+    #         playlist_id = playlist['id']
 
-    print(response.status_code)
-    print(f'Playlist can be found at {url}')
+    # Filling new playlist with recommended songs
+    # if playlist_id is None:
+    #     print('99999999999')
+    # else:
+    #     print(playlist_id)
+    # endpoint_url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+    # request_body = json.dumps({
+    #     "myPlaylist" : myPlaylist
+    # })
 
     song_id_list = []
 
-    for i in json_response['tracks']:
-        track_id = spotify.search(q='artist:' + i['artists'][0]['name'] + ' track:' + i['name'], type='track')
+    for i, j in enumerate(json_response.get('tracks', [])):
+        track_id = spotify.search(q='artist:' + j['artists'][0]['name'] + ' track:' + j['name'], type='track')
         items = track_id['tracks']['items']
+        trackid = ''
         if len(items) > 0:
             trackid = items[0]
+        else:
+            print('000000000000000000000000000000000000')
         song_id_list.append(trackid['id'])
+        print(song_id_list[i])
 
-    for j in song_id_list:
-        spotify.current_user().user_playlist_add_tracks(index().current_user(), playlist_id, tracks = [j])
+    for i in song_id_list:
+        song = [i]
+        spotify.user_playlist_add_tracks(spotify.current_user(), playlist_id, song)
 
     song_id_list.clear()
+
+    webbrowser.open("https://open.spotify.com/")
+    return 'OK'
 
 if __name__ == '__main__':
     app.run()

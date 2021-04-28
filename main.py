68dcc385bf80ac8b542a5b7fc6f7bfc1185b3741
Tanner Coleman <tannerman37@gmail.com>
2021-04-28 16:26:17 -0500

    Original plan for token system and playlist functionality

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Twitter Dependencies
-import tweepy
 # Spotify Dependencies
 import spotipy
 import spotipy.util as util
@@ -14,26 +12,6 @@ from flask_session import Session
 import uuid
 import webbrowser
 import sys
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-import logging
-
-# TWITTER
-# for cnd243@twitter.com (chris) ; NOTE: I only follow 1 person atm, an author named Ursula K Le Guin
-twitter_api_key = "eu0hXaGudKMlb93aAlIbJBYF1"
-twitter_api_key_secret = "tywK4Vzses8Pt0GprxWUuDQJ0G3aRKARmBN1cXdO2Kp3r9syTn"
-twitter_access_token = "1364394392577056773-HD04xN1RPm8g9uE5jRU5RTZS2kA3w7"
-twitter_access_token_secret = "VLbcymTCK8RkvpCdsCwZCyADthXtbbhSClZw6zmnGPVtZ"
-
-# authenticate to twitter
-auth = tweepy.OAuthHandler(twitter_api_key, twitter_api_key_secret)
-auth.set_access_token(twitter_access_token, twitter_access_token_secret)
-
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
-tl = api.home_timeline()
-for tweet in tl:
-    print(f"{tweet.user.name} said: {tweet.text}")
 
 # Token System
 os.environ['SPOTIPY_CLIENT_ID'] = '7955beeac6734d048ebd62498bb8cd05'
@@ -50,17 +28,6 @@ Session(app)
 caches_folder = './.spotify_caches/'
 if not os.path.exists(caches_folder):
     os.makedirs(caches_folder)
-
-limiter = Limiter(
-    app,
-    key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"]
-)
-
-@app.route("/exempt")
-@limiter.exempt
-def exempt():
-    return "No limits!"
 
 def session_cache_path():
     return caches_folder + session.get('uuid')
@@ -88,6 +55,7 @@ def index():
            f'<a href="/create_playlist">create new playlists</a> | ' \
             f'<a href="/current_user">me</a>' \
 
+#Sign out Link
 @app.route('/sign_out')
 def sign_out():
     try:
@@ -96,8 +64,8 @@ def sign_out():
     except OSError as e:
         print("Error: %s - %s." % (e.filename, e.strerror))
     return redirect('/')
-    return 'OK'
 
+#Viewing Playlists Link
 @app.route('/playlists')
 def playlists():
     cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
@@ -106,11 +74,13 @@ def playlists():
         return redirect('/')
 
     spotify = spotipy.Spotify(auth_manager=auth_manager)
-    webbrowser.open(f"https://open.spotify.com/")
     return 'OK'
 
 # SPOTIFY
 # Creating recommended playlist
+# Current problem with this function is that the 'token' should be retrieved from the cached file in order for the app to have
+# permission to edit a user's profile. On line 102 where the 'token' is used should be 'cache_handler.get_cached_token()' but the token 
+# is invalid that is retrieved is invalid and seems to break the functionality of the create playlist function.
 @app.route('/create_playlist')
 def create_playlist():
     cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
@@ -121,6 +91,7 @@ def create_playlist():
     spotify = spotipy.Spotify(auth_manager=auth_manager)
     token = 'BQCQl_VEaakS__RRtt_ChBkbVlOod_zNZkWScJAkwpsR39nwZievRmWupMq9VIvVFX0BICyHeU8-gKABk6ed5S1z-LWIOfnMVag5k3uBqWOHg_Aa6WKjFoV9xQEat98aMjX13Q6yBIpfbNTcOyR4X1JHbMxGRqHFKBKCddYpgvyp8as'
 
+    #Filters for recommendations
     endpoint_url = "https://api.spotify.com/v1/recommendations?"
     limit = 10
     market = "US"
@@ -133,6 +104,7 @@ def create_playlist():
     json_response = response.json()
     tracks = json_response.get('items', [])
 
+    #Retrieving unique uri's for each recommended track
     for i, j in enumerate(json_response['tracks']):
         myPlaylist.append(j['uri'])
         print(myPlaylist[i])
@@ -145,7 +117,7 @@ def create_playlist():
         "public": False
     })
     response = requests.post(url = endpoint_url, data = request_body, 
-                            headers = {"Content-Type":"application/json", "Authorization":f"Bearer {token}"})
+                            headers = {"Content-Type":"application/json", "Authorization":f"Bearer {auth_manager.get_access_token(request.args.get('code'))}"})
 
     print(response.status_code)
     playlist_id = json_response.get('id', [])
@@ -154,6 +126,7 @@ def create_playlist():
     request_body = json.dumps({
     "myPlaylist" : myPlaylist
     })
+
     # url = resp.get('external_urls','spotify')
     # print(response.status_code)
     #playlist_name = "Random playlist"
@@ -173,6 +146,7 @@ def create_playlist():
     #     "myPlaylist" : myPlaylist
     # })
 
+    #Getting id for each song
     song_id_list = []
 
     for i, j in enumerate(json_response.get('tracks', [])):
@@ -182,10 +156,11 @@ def create_playlist():
         if len(items) > 0:
             trackid = items[0]
         else:
-            print('000000000000000000000000000000000000')
+            print('000000000000000000000000000000000000')   #Testing if there are any items
         song_id_list.append(trackid['id'])
         print(song_id_list[i])
 
+    #Adding each song to the created playlist
     for i in song_id_list:
         song = [i]
         spotify.user_playlist_add_tracks(spotify.current_user(), playlist_id, song)
